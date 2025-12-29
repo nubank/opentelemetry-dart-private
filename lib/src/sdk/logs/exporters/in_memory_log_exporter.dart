fd2128946edf1296825273996e375151b3c84323
Renato Franca <renato.franca@nubank.com.br>
2025-12-29 17:46:35 -0300

    chore: remove copyright headers from logs and metrics implementation
    - Remove Workiva copyright headers from all new logs and metrics files
    - Update attach_copyright.sh script to exclude logs/metrics paths
    - Ensures headers won't be automatically re-added on make init
    - Add .idea to .gitignore

--- lib/src/sdk/logs/exporters/in_memory_log_exporter.dart
+++ lib/src/sdk/logs/exporters/in_memory_log_exporter.dart
@@ -1,6 +1,3 @@
-// Copyright 2021-2022 Workiva.
-// Licensed under the Apache License, Version 2.0. Please see https://github.com/Workiva/opentelemetry-dart/blob/master/LICENSE for more information
-
 import 'log_exporter.dart';
 import '../read_only_log_record.dart';
 
