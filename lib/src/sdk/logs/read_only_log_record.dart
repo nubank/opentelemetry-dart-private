2d00b7b19288767a48a4a5b237b8a76e41be514e
Renato Franca <renato.franca@nubank.com.br>
2025-12-29 17:42:26 -0300

    chore: add copyright headers to new files
    Automatically added by scripts/attach_copyright.sh as part of make init

--- lib/src/sdk/logs/read_only_log_record.dart
+++ lib/src/sdk/logs/read_only_log_record.dart
@@ -1,3 +1,6 @@
+// Copyright 2021-2022 Workiva.
+// Licensed under the Apache License, Version 2.0. Please see https://github.com/Workiva/opentelemetry-dart/blob/master/LICENSE for more information
+
 import 'package:fixnum/fixnum.dart';
 import '../../../api.dart' as api;
 import '../../../sdk.dart' as sdk;
--- lib/src/sdk/metrics/exporters/metric_exporter.dart
+++ lib/src/sdk/metrics/exporters/metric_exporter.dart
@@ -1,3 +1,6 @@
+// Copyright 2021-2022 Workiva.
+// Licensed under the Apache License, Version 2.0. Please see https://github.com/Workiva/opentelemetry-dart/blob/master/LICENSE for more information
+
 import '../metric_data.dart';
 
 /// MetricExporter is responsible for exporting metric data
